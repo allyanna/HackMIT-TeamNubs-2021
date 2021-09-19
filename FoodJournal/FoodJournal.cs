10e6a088a2c2ff904d58d967dc2f82617a4e9fe1
allyanna <u1223264@eng.utah.edu>
2021-09-18 20:25:20 -0600

    Add project files.

---
+++ FoodJournal/FoodJournal.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FoodJournal
+{
+    public class FoodJournal
+    {
+        private Dictionary<int, JournalEntry> journal;
+        public FoodJournal()
+        {
+            journal = new Dictionary<int, JournalEntry>();
+        }
+
+       
+    }
+}
---
+++ FoodJournal/JournalEntry.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodJournal
+{
+    public class JournalEntry
+    {
+        /*stores an id of the journal entry*/
+        private int ID;
+        /*stores the date of entry*/
+        private DateTime date;
+        /*mealtime ex. dinner*/
+        private string mealtime;
+        /*list of foods*/
+        private List<string> food;
+        /*symptoms for meal*/
+        private List<string> symptoms;
+        /*personal rating of healthiness*/
+        private int healthiness;
+        /*personal rating of satisfaction*/
+        private int satisfaction;
+
+        /// <summary>
+        /// ID, date, mealtime, food, symptoms, healthiness, satisfaction
+        /// </summary>
+        public JournalEntry()
+        {
+
+        }
+
+        /// <summary>
+        /// ID, date, mealtime, food, symptoms, healthiness, satisfaction
+        /// </summary>
+        public JournalEntry(int ID, DateTime date, string mealtime, List<string> food, List<string> symptoms, int healthiness, int satisfaction)
+        {
+            this.ID = ID;
+            this.date = date;
+            this.mealtime = mealtime;
+            this.food = food;
+            this.symptoms = symptoms;
+            this.healthiness = healthiness;
+            this.satisfaction = satisfaction;
+        }
+
+
+
+
+    }
+}
