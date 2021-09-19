632625c3eefac55e8ac7f81066cd204bd63b92c2
Lauren Schwenke <u1117638@umail.utah.edu>
2021-09-19 06:17:19 -0600

    Can read from txt files with Json

--- FoodJournal1/Journal.cs
+++ FoodJournal1/Journal.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Windows.Forms;
 
 namespace FoodJournal
 {
@@ -20,12 +21,29 @@ namespace FoodJournal
         /// 
         /// </summary>
         /// <param name="filename"></param>
-        public void readFiles(string filename) { 
-        //TODO: to tell user if it was successful or not
-            using (StreamReader sr = new StreamReader(filename))
+        public void readFiles(string filename) {
+            //TODO: to tell user if it was successful or not
+            using (OpenFileDialog ofd = new OpenFileDialog())
             {
-                string json = sr.ReadToEnd();
-                List<JournalEntry> entries = JsonConvert.DeserializeObject<List<JournalEntry>>(json);
+                ofd.Filter = "txt files (*.txt)|*.txt|All files|*.*";
+
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    string filepath = ofd.FileName;
+                    var filestream = ofd.OpenFile(); // HMMMMMM
+
+                    using (StreamReader sr = new StreamReader(filename))
+                    {
+                        string json = sr.ReadToEnd();
+                        List<JournalEntry> entries = JsonConvert.DeserializeObject<List<JournalEntry>>(json);
+
+                        foreach (JournalEntry entry in entries)
+                        {
+                            Console.WriteLine(entry.getMealtime());
+
+                        }
+                    }
+                }
             }
 
         
